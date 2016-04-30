202fda35e87a7bc041b1efc6b2cf7d3f0a5833f2
Maxime Déraspe <maximilien1er@gmail.com>
2016-04-29 19:49:53 -0700

    add mold network with sigmajs
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- js/main.js
+++ js/main.js
@@ -22,13 +22,6 @@ var yasr = YASR(document.getElementById("yasr"), {
 //link both together
 yasqe.options.sparql.callbacks.complete = yasr.setResponse;
 
-// hack to load yasqe correctly :
-//    first tab is query the move to about tab
-$(document).ready(function(){
-  $("#main_tabs a[href='#about']").tab("show");
-  $('#virtuosoiframe').attr('src', fct_url);
-})
-
 /* HEADER fun */
 var toggleHeader = function() {
   $('#header').toggle();
@@ -41,7 +34,7 @@ var showHeader = function() {
 }
 
 /* SEARCH */
-$(document).ready(function(){
+var load_search = function() {
   var submitIcon = $('.searchbox-icon');
   var inputBox = $('.searchbox-input');
   var searchBox = $('.searchbox');
@@ -69,7 +62,7 @@ $(document).ready(function(){
       submitIcon.click();
     }
   });
-});
+};
 
 var current_search_results = {}
 var current_ajax_call = null;
@@ -77,7 +70,7 @@ var current_ajax_call = null;
 function buttonUp(){
 
   var inputVal = $('.searchbox-input').val();
-  
+
   inputValLen = $.trim(inputVal).length;
 
   if( inputValLen !== 0){
@@ -88,7 +81,7 @@ function buttonUp(){
         url: api_url + '/v1/search?q=' + inputVal,
         type: 'GET',
         dataType: 'json',
-        beforeSend : function()    {           
+        beforeSend : function()    {
           if(current_ajax_call != null) {
             current_ajax_call.abort();
           }
@@ -127,3 +120,95 @@ $('#mysearch').submit(function(e) {
     console.log("no object found !! ") // TODO submit post to virtuoso
   }
 });
+
+/* Network graph */
+// Add a method to the graph model that returns an
+// object with every neighbors of a node inside:
+
+var load_mold_network = function(){
+
+  sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index)
+      neighbors[k] = this.nodesIndex[k];
+
+    return neighbors;
+  });
+
+  sigma.parsers.gexf(
+    '../config/mold-network.gexf',
+    {
+      container: 'mold_sigma'
+    },
+    function(s) {
+      // We first need to save the original colors of our
+      // nodes and edges, like this:
+      s.graph.nodes().forEach(function(n) {
+        n.originalColor = n.color;
+      });
+      s.graph.edges().forEach(function(e) {
+        e.originalColor = e.color;
+      });
+
+      // When a node is clicked, we check for each node
+      // if it is a neighbor of the clicked one. If not,
+      // we set its color as grey, and else, it takes its
+      // original color.
+      // We do the same for the edges, and we only keep
+      // edges that have both extremities colored.
+      s.bind('clickNode', function(e) {
+        var nodeId = e.data.node.id,
+            toKeep = s.graph.neighbors(nodeId);
+        toKeep[nodeId] = e.data.node;
+
+        s.graph.nodes().forEach(function(n) {
+          if (toKeep[n.id])
+            n.color = n.originalColor;
+          else
+            n.color = '#eee';
+        });
+
+        s.graph.edges().forEach(function(e) {
+          if (toKeep[e.source] && toKeep[e.target])
+            e.color = e.originalColor;
+          else
+            e.color = '#eee';
+        });
+
+        // Since the data has been modified, we need to
+        // call the refresh method to make the colors
+        // update effective.
+        s.refresh();
+      });
+
+      // When the stage is clicked, we just color each
+      // node and edge with its original color.
+      s.bind('clickStage', function(e) {
+        s.graph.nodes().forEach(function(n) {
+          n.color = n.originalColor;
+        });
+
+        s.graph.edges().forEach(function(e) {
+          e.color = e.originalColor;
+        });
+
+        // Same as in the previous event:
+        s.refresh();
+      });
+    }
+  );
+
+
+};
+
+// hack to load yasqe correctly :
+//    first tab is query the move to about tab
+$(document).ready(function(){
+  $("#main_tabs a[href='#about']").tab("show");
+  $('#virtuosoiframe').attr('src', fct_url);
+  load_search();
+  window.setTimeout(function (){  load_mold_network() }, 500);
+})
