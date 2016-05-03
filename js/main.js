f2d5ea159edd39400b438d85fafe03074134a143
Maxime Déraspe <maximilien1er@gmail.com>
2016-05-02 22:13:26 -0700

    fix glitch WebGL-SVG with jquery timeout .. weird hack
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- js/main.js
+++ js/main.js
@@ -8,22 +8,6 @@ var fct_url = virtuoso_url + "/fct";
 var describe_url = virtuoso_url + "/describe";
 
 
-/* YASQE & YASR SPARQL */
-var yasqe = YASQE(document.getElementById("yasqe"), {
-  sparql: {
-    showQueryButton: true,
-    endpoint: sparql_url,
-    requestMethod: "GET"
-  }
-});
-var yasr = YASR(document.getElementById("yasr"), {
-  //this way, the URLs in the results are prettified using the defined prefixes in the query
-  getUsedPrefixes: yasqe.getPrefixesFromQuery
-});
-
-//link both together
-yasqe.options.sparql.callbacks.complete = yasr.setResponse;
-
 /* HEADER fun */
 var toggleHeader = function() {
   $('#header').toggle();
@@ -126,25 +110,20 @@ $('#mysearch').submit(function(e) {
 /* Network graph */
 // Add a method to the graph model that returns an
 // object with every neighbors of a node inside:
-
+var graphInstance = {container:'mold_sigma'};
 var load_mold_network = function(){
-
   sigma.classes.graph.addMethod('neighbors', function(nodeId) {
     var k,
         neighbors = {},
         index = this.allNeighborsIndex[nodeId] || {};
-
     for (k in index)
       neighbors[k] = this.nodesIndex[k];
-
     return neighbors;
   });
 
   sigma.parsers.gexf(
     '../config/mold-network.gexf',
-    {
-      container: 'mold_sigma'
-    },
+    graphInstance,
     function(s) {
       // We first need to save the original colors of our
       // nodes and edges, like this:
@@ -154,7 +133,6 @@ var load_mold_network = function(){
       s.graph.edges().forEach(function(e) {
         e.originalColor = e.color;
       });
-
       // When a node is clicked, we check for each node
       // if it is a neighbor of the clicked one. If not,
       // we set its color as grey, and else, it takes its
@@ -165,46 +143,47 @@ var load_mold_network = function(){
         var nodeId = e.data.node.id,
             toKeep = s.graph.neighbors(nodeId);
         toKeep[nodeId] = e.data.node;
-
         s.graph.nodes().forEach(function(n) {
           if (toKeep[n.id])
             n.color = n.originalColor;
           else
             n.color = '#eee';
         });
-
         s.graph.edges().forEach(function(e) {
           if (toKeep[e.source] && toKeep[e.target])
             e.color = e.originalColor;
           else
             e.color = '#eee';
         });
-
         // Since the data has been modified, we need to
         // call the refresh method to make the colors
         // update effective.
         s.refresh();
       });
-
       // When the stage is clicked, we just color each
       // node and edge with its original color.
       s.bind('clickStage', function(e) {
         s.graph.nodes().forEach(function(n) {
           n.color = n.originalColor;
         });
-
         s.graph.edges().forEach(function(e) {
           e.color = e.originalColor;
         });
-
         // Same as in the previous event:
         s.refresh();
       });
     }
   );
-
 };
 
+var refresh_mold_network = function() {
+  sigma.parsers.gexf(
+    '../config/mold-network.gexf',
+    graphInstance,
+    function(s){
+      s.refresh();
+    })
+}
 
 // hack to load yasqe correctly :
 //    first tab is query the move to about tab
@@ -212,20 +191,46 @@ $(document).ready(function(){
   $("#main_tabs a[href='#about']").tab("show");
   $('#virtuosoiframe').attr('src', fct_url);
   load_search();
-  window.setTimeout(function (){  load_mold_network() }, 500);
+  window.setTimeout(function (){
+    load_mold_network();
+  }, 1000);
 })
 
+var queryExecuted = false;
 var refreshAbout = function() {
   showHeader()
-  window.setTimeout(function (){  load_mold_network() }, 1000);
+  // weird this timeout below and bool setting remove the glitch WebGL/SVG(d3)
+  if (queryExecuted) {
+    window.setTimeout(function (){
+      queryExecuted = false;
+    }, 1000);
+  }
 }
 
+/*sticky nav-bar*/
 var stickyOffset = $('.sticky').offset().top;
-
 $(window).scroll(function(){
   var sticky = $('.sticky'),
       scroll = $(window).scrollTop();
-
   if (scroll >= stickyOffset) sticky.addClass('fixed');
   else sticky.removeClass('fixed');
 });
+
+/* YASQE & YASR SPARQL */
+var yasqe = YASQE(document.getElementById("yasqe"), {
+  sparql: {
+    showQueryButton: true,
+    endpoint: sparql_url,
+    requestMethod: "GET"
+  }
+});
+var yasr = YASR(document.getElementById("yasr"), {
+  //this way, the URLs in the results are prettified using the defined prefixes in the query
+  getUsedPrefixes: yasqe.getPrefixesFromQuery
+});
+var queryCallback =   function () {
+  queryExecuted = true;
+  return yasr.setResponse;
+}
+//link both together
+yasqe.options.sparql.callbacks.complete = queryCallback();
